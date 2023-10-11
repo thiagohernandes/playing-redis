9f6cba3a441e3f5a2d562cfde32c2771f38a0be5
Thiago Hernandes de Souza <thiago.hernandes@dbccompany.com.br>
2023-10-11 10:41:21 -0300

    Criação do projeto

---
+++ src/main/java/com/playing/api/PlayingRedisApplication.java
@@ -0,0 +1,13 @@
+package com.playing.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playing/api/PlayingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playing.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
